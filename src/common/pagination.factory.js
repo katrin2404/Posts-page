e14c7bb71552a7951e6640f9eb5a160b62961c56
Katsiaryna_Sachko <sachko.ekaterina@gmail.com>
2018-01-15 16:19:41 +0300

    fix: current page for filter

--- src/common/pagination.factory.js
+++ src/common/pagination.factory.js
@@ -1,23 +1,24 @@
 (function () {
-    'use strict';
-    angular.module('postsList').factory('Pagination', Pagination);
+  'use strict';
+  angular.module('postsList').factory('Pagination', Pagination);
 
-    Pagination.$inject = ['$filter'];
+  Pagination.$inject = ['$filter'];
 
-    function Pagination($filter) {
-        return {
-            getData,
-            numberOfPages,
-        };
+  function Pagination($filter) {
+    return {
+      getData,
+      numberOfPages,
+    };
 
-        function getData(posts, search) {
-            return $filter('filter')(posts, search);
-        }
 
-        function numberOfPages(data, pageSize) {
-            let pages = Math.ceil(data.length / pageSize);
-            return pages === 0 ? 1 : pages;
-        }
+    function getData(posts, search) {
+      return $filter('filter')(posts, search);
     }
+
+    function numberOfPages(data, pageSize) {
+      let pages = Math.ceil(data.length / pageSize);
+      return pages === 0 ? 1 : pages;
+    }
+  }
 
 })();
--- src/posts-list/posts-list.controller.js
+++ src/posts-list/posts-list.controller.js
@@ -1,28 +1,29 @@
 (function () {
 
-    'use strict';
-    angular.module('postsList').controller('PostsListController', PostsListController);
+  'use strict';
+  angular.module('postsList').controller('PostsListController', PostsListController);
 
-    PostsListController.$inject = ['Pagination', 'PostsListService'];
+  PostsListController.$inject = ['Pagination', 'PostsListService'];
 
-    function PostsListController(Pagination, PostsListService) {
-        const vm = this;
+  function PostsListController(Pagination, PostsListService) {
+    const vm = this;
 
-        vm.posts = [];
-        vm.orderProp = 'title';
-        vm.currentPage = 0;
-        vm.pageSize = 10;
-        vm.searchPost = '';
-        vm.getData = Pagination.getData;
-        vm.numberOfPages = Pagination.numberOfPages;
+    vm.posts = [];
+    vm.orderProp = 'title';
+    vm.currentPage = 0;
+    vm.pageSize = 10;
+    vm.searchPost = '';
+    vm.getData = Pagination.getData;
+    vm.numberOfPages = Pagination.numberOfPages;
 
-        init();
 
-        function init() {
-            return PostsListService
-                .loadPosts()
-                .then(posts => vm.posts = posts);
-        }
+    init();
+
+    function init() {
+      return PostsListService
+        .loadPosts()
+        .then(posts => vm.posts = posts);
     }
+  }
 
 })();
